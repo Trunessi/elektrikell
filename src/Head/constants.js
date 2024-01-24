d0ca319cae369728d996b767514cec88179b2875
Nina Naumenko <tru.nessi@gmail.com>
2024-01-24 21:07:21 +0200

    ui react-bootstrap & activePrice

---
+++ src/Head/constants.js
@@ -0,0 +1,12 @@
+export const PRICE_BUTTONS = [
+  {
+    name: "Low Price",
+    id: "Low",
+  },
+  {
+    name: "High Price",
+    id: "High",
+  },
+];
+
+export const DEFAULT_ACTIVE_BUTTONS = "Low";
