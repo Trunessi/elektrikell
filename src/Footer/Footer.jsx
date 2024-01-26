2565102a0952e7f961fd7b3fc501c9c57e9930de
Nina Naumenko <tru.nessi@gmail.com>
2024-01-26 18:25:03 +0200

    homework2 after pull request

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,8 +1,10 @@
 import TargetHigh from "./TargetHigh";
 import TargetLow from "./TargetLow";
+import { DEFAULT_ACTIVE_BUTTONS } from "../Head";
+
 
 function Footer(props) {
-  return <div className="footer">{props.activePrice === "High" ? TargetHigh() : TargetLow()}</div>;
+  return <div className="footer">{props.activePrice === DEFAULT_ACTIVE_BUTTONS ? <TargetLow/> : <TargetHigh/>}</div>;
 }
 
 export default Footer;
