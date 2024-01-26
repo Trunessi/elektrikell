0e7c91a16a62e34b69f9b926743c45db51bed848
Trunessi <92943505+Trunessi@users.noreply.github.com>
2024-01-26 18:29:39 +0200

    low/high price footer & css (#2)
    * low/high price footer & css
    
    * changes after review

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,5 +1,10 @@
-function Footer() {
-    return (<></>);
+import TargetHigh from "./TargetHigh";
+import TargetLow from "./TargetLow";
+import { DEFAULT_ACTIVE_BUTTONS } from "../Head";
+
+
+function Footer(props) {
+  return <div className="footer">{props.activePrice === DEFAULT_ACTIVE_BUTTONS ? <TargetLow/> : <TargetHigh/>}</div>;
 }
 
 export default Footer;
