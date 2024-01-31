bda7efd5e88b7b094708e19387e86fd51b633eb1
Nina Naumenko <tru.nessi@gmail.com>
2024-01-31 17:17:59 +0200

    getCurrentPrice , currentPriceData

--- src/Head/Info.jsx
+++ src/Head/Info.jsx
@@ -1,11 +1,21 @@
+import { useEffect, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import { PRICE_BUTTONS, BADGES } from "./constants";
 import Badge from 'react-bootstrap/Badge';
+import { getCurrentPrice } from '../services/apiService'
 
 
 function Info({ activePrice, setActivePrice }) {
+  const [currentPriceData, setCurrentPriceData] = useState(null);
+
+  useEffect(() => {
+    getCurrentPrice().then(({ data }) => 
+        setCurrentPriceData(data[0].price) 
+      );
+  }, []);
+
   return (
     <>
       <Col>
@@ -27,7 +37,7 @@ function Info({ activePrice, setActivePrice }) {
         </ButtonGroup>
       </Col>
       <Col className="text-end">
-        <h2>XX.XX</h2>
+        <h2>{currentPriceData}</h2>
         <div>cent / kilovatt-hour</div>
       </Col>
     </>
