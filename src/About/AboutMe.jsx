339db6a746d25aafbbc95366112b440c9eb4707a
Nina Naumenko <tru.nessi@gmail.com>
2024-02-15 16:50:53 +0200

    about pages

---
+++ src/About/AboutMe.jsx
@@ -0,0 +1,16 @@
+function AboutMe() {
+  return (
+    <div className="aboutMe container">
+      <p>
+        My name is Nina Naumenko. I'm learning front-end development, especially
+        React framework. My higher education in IT and many years of experience
+        as a brand designer help me with this.
+      </p>
+      <p>
+        <a href="https://github.com/Trunessi"> My GitHub</a>
+      </p>
+    </div>
+  );
+}
+
+export default AboutMe;
